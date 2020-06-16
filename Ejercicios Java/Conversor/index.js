5d91ac20030e8acf645fbbc3990ba0413f2ad245
Lina Gonzalez <lmgb15@gmail.com>
2020-06-15 20:45:50 -0500

    ..

---
+++ Ejercicios Java/Conversor/index.js
@@ -0,0 +1,5 @@
+function centigradoAFarenheit(centigrados{
+   var resultado =Math.round((centigrado*9/5) + 32;)
+   console.log(resultado)
+}
+centigradoAFarenheit(16)
---
+++ Ejercicios Java/index.js
@@ -0,0 +1,8 @@
+function mayorDeEdad(edad){
+    if(edad >= 18){
+        console.log("Eres mayor de edad")
+    } else{
+        console.log("No eres mayor de edad")
+    }
+}
+MayorDeEdad(15)
